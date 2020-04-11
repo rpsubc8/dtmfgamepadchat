95b346c320ce5ba0f331b2d20ad40e628c6cdf7b
Jaime Jose Gavin Sierra <42617981+rpsubc8@users.noreply.github.com>
2020-04-11 12:17:32 +0200

    Add external Relay PTT, timeout and silence detect

--- Arduino/leonardoJoystick/leonardoJoystick.ino
+++ Arduino/leonardoJoystick/leonardoJoystick.ino
@@ -11,13 +11,29 @@
 // Mod Jaime Jose Gavin Sierra
 // EA1HLX
 // DTMF MT8870 pin to gamepad buttons
-// Arduino Leonrado
+// Arduino Leonardo
+//  time_out_relay (Maximo tiempo de PTT activo en ms)
+//  time_silence (Maximo tiempo de deteccion de silencio en ms)
+//  time_delay (tiempo unidad principal de ms en espera)
 //--------------------------------------------------------------------
+//Usar use_relay_trigger_low o use_relay_trigger_high (no los 2 al mismo tiempo)
+//Para activar rele por logica negada o no
+#define use_relay_trigger_low
+//#define use_relay_trigger_high
+
 #define pin_dtmf_stq 2
 #define pin_dtmf_q4 3
 #define pin_dtmf_q3 4
 #define pin_dtmf_q2 5
 #define pin_dtmf_q1 6
+#define pin_dtmf_relay 7
+
+//Delay milliseconds
+#define time_delay 100
+//Maximo tiempo rele activo Timeout milisegundos 120 segundos (2 minutos) * 1000 ms = 120000 ms
+#define time_out_relay 120000
+//Tiempo maximo de silencio milisegundos 1 segundo 1000 ms
+#define time_silence 1000
 
 //stq 3
 //Q3 5
@@ -30,12 +46,18 @@
 #define pad_q2 8
 #define pad_q1 9
 #define pad_switchFlip 10 //boton flipflop pulsado para activar navegador
+#define pad_relay 11 //boton indicando rele
 
 #include <Joystick.h>
 
 byte stq,q4,q3,q2,q1;
 bool switchFlip = false;
 byte cont_switchFlip=0;
+bool activarRelay = false;
+bool DTMFactivo = false;
+unsigned long relay_cont_time=0;
+unsigned long relay_time_fin=0;
+unsigned long silencio_cont_time=0;
 Joystick_ Joystick;
 
 void setup() {  
@@ -44,8 +66,17 @@ void setup() {
   pinMode(pin_dtmf_q4, INPUT);
   pinMode(pin_dtmf_q3, INPUT);
   pinMode(pin_dtmf_q2, INPUT);
-  pinMode(pin_dtmf_q1, INPUT); 
+  pinMode(pin_dtmf_q1, INPUT);
+  pinMode(pin_dtmf_q1, INPUT);
+  pinMode(pin_dtmf_relay, OUTPUT);
 
+  #ifdef use_relay_trigger_low
+   digitalWrite(pin_dtmf_relay,HIGH); //Dejar bajo  
+  #endif
+  #ifdef use_relay_trigger_high
+   digitalWrite(pin_dtmf_relay,LOW); //Dejar bajo
+  #endif
+  
   // Initialize Joystick Library
   Joystick.begin();
 }
@@ -55,6 +86,20 @@ void setup() {
 
 // Last state of the button
 //int lastButtonState[4] = {0,0,0,0};
+
+void DesactivarPTT()
+{
+ silencio_cont_time = 0;
+ relay_cont_time = 0;
+ activarRelay = false; 
+ #ifdef use_relay_trigger_low
+  digitalWrite(pin_dtmf_relay, HIGH); //Desactivar Rele
+ #endif
+ #ifdef use_relay_trigger_high
+  digitalWrite(pin_dtmf_relay, LOW); //Desactivar Rele
+ #endif
+ Joystick.setButton(pad_relay, LOW);
+}
 
 void loop() {
 
@@ -76,10 +121,15 @@ void loop() {
    cont_switchFlip = 0;
    switchFlip = !switchFlip;
   }
-  if (switchFlip == true)  
+  if (switchFlip == true)
+  {  
    Joystick.setButton(pad_switchFlip, HIGH);
-  else 
+  }
+  else
+  {
    Joystick.setButton(pad_switchFlip, LOW);
+  }
+  
   q4 = digitalRead(pin_dtmf_q4);
   Joystick.setButton(pad_q4, q4);
   q3 = digitalRead(pin_dtmf_q3);
@@ -90,6 +140,51 @@ void loop() {
   Joystick.setButton(pad_q1, q1);
   stq  = digitalRead(pin_dtmf_stq);
   Joystick.setButton(pad_stq, stq);
+  
+  //DTMF C es 15 1111
+  if (q1 == HIGH && q2 == HIGH && q3 == HIGH && q4 == HIGH && stq == HIGH)
+  {
+   activarRelay = true;   
+   relay_cont_time = 0;
+  }
 
-  delay(100);
+  DTMFactivo = (stq == HIGH)?true:false;    
+
+  if (activarRelay == true)
+  {
+   relay_cont_time ++;
+   #ifdef use_relay_trigger_low
+    digitalWrite(pin_dtmf_relay, LOW); //Activar Rele
+   #endif    
+   #ifdef use_relay_trigger_high
+    digitalWrite(pin_dtmf_relay, HIGH); //Activar Rele
+   #endif
+   Joystick.setButton(pad_relay, HIGH);
+   relay_time_fin = relay_cont_time * time_delay; //milisegundos   
+   if (relay_time_fin > time_out_relay)
+   {
+    DesactivarPTT();
+   }
+   else
+   {
+    if (DTMFactivo == false)
+    {
+     silencio_cont_time ++;
+     if ((silencio_cont_time * time_delay) > time_silence)
+     {
+      DesactivarPTT();
+     }
+    }
+    else
+    {
+      silencio_cont_time = 0; //reseteamos silencio    
+    }
+   }
+  }
+  else
+  {   
+   DesactivarPTT();
+  }
+
+  delay(time_delay);
 }
